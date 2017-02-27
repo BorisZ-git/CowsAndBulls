799227c2e1437f63cf9bc0fe04fdb94544d7ba98
BorisZ-git <ribos@bk.ru>
2017-02-27 15:06:42 +0300

    create scheme of function Сonsequences

--- CowsAndBulls/CowsAndBulls/Choose.cs
+++ CowsAndBulls/CowsAndBulls/Choose.cs
@@ -8,5 +8,37 @@ namespace CowsAndBulls
 {
     class Choose
     {
+        static public Array Сonsequences()
+        {
+            //Choose
+            Console.Write("Choose your enemy:\n1.Pc\t2.Friend");
+            int Enemy = int.Parse(Console.ReadLine());
+            //Сonsequences of choosing(последствия выбора)
+            for (int i = 0; i < 0;)
+            {
+                //PC use random
+                if (Enemy == 1)
+                {
+                    for (int a = 0; a < RightAnswer.Count; a++) ;
+                    Random rnd = new Random();
+                    int RightAnswer[];
+                    return RightAnswer;
+                }
+                //User take number in array
+                if (Enemy == 2)
+                {
+                    return RightAnswer;
+                }
+                //Catch wrong answer
+                else Console.WriteLine("You need enter the number: ");
+            }
+           
+        }
+        static public int Attempt()
+        {
+            Console.Write("Enter attempts: ");
+            int Attempt = int.Parse(Console.ReadLine());
+            return Attempt;
+        }
     }
 }
