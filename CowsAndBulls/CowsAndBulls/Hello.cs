ea0a552e545600cfbed162586374e18016b02a4f
BorisZ-git <ribos@bk.ru>
2017-02-27 14:52:29 +0300

    Create class Choose\\ Where will be realize taking number and value of attempts

--- CowsAndBulls/CowsAndBulls/Hello.cs
+++ CowsAndBulls/CowsAndBulls/Hello.cs
@@ -8,5 +8,15 @@ namespace CowsAndBulls
 {
     class Hello
     {
+        static public void Rule()
+        {
+            Console.WriteLine("You choose who will be your enemy: PC or friend.\n" );
+            Console.Write("Your enemy wish a number like as 1234.\n");
+            Console.Write("You will have several attempts to answer right.\n");
+            Console.Write("When you will guess right number, but not position. You'll take a cow\n");
+            Console.Write("When you will guess position, but not a number. You'll take a bull\n");
+            Console.Write("So if enemy wish number 1234, and your answer 1567. You have 1 cow and 1 bull");
+        }
+
     }
 }
